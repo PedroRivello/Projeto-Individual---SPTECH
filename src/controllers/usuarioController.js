a5830a6f5db65908b0b65e42a24b4311638fb06a
PedroRivello <pedro.rivello@sptech.school>
2026-05-12 23:51:27 -0300

    Conectando banco para receber os registros dos usuarios, rodando API, criacao da Dashboard com dados mocados com estilização

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,101 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function autenticar(req, res) {
+
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    if (email == undefined) {
+
+        res.status(400).send("Seu email está undefined!");
+
+    } else if (senha == undefined) {
+
+        res.status(400).send("Sua senha está undefined!");
+
+    } else {
+
+        usuarioModel.autenticar(email, senha)
+
+            .then(function (resultadoAutenticar) {
+
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+
+                if (resultadoAutenticar.length == 1) {
+
+                    res.json({
+                        idUsuario: resultadoAutenticar[0].idUsuario,
+                        nome: resultadoAutenticar[0].nome,
+                        email: resultadoAutenticar[0].email,
+                        senha: resultadoAutenticar[0].senha,
+                        dataCadastro: resultadoAutenticar[0].dataCadastro
+                    });
+
+                } else if (resultadoAutenticar.length == 0) {
+
+                    res.status(403).send("Email e/ou senha inválido(s)");
+
+                } else {
+
+                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+
+                }
+
+            }).catch(function (erro) {
+
+                console.log(erro);
+
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+
+                res.status(500).json(erro.sqlMessage);
+
+            });
+    }
+}
+
+function cadastrar(req, res) {
+
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    if (nome == undefined) {
+
+        res.status(400).send("Seu nome está undefined!");
+
+    } else if (email == undefined) {
+
+        res.status(400).send("Seu email está undefined!");
+
+    } else if (senha == undefined) {
+
+        res.status(400).send("Sua senha está undefined!");
+
+    } else {
+
+        usuarioModel.cadastrar(nome, email, senha)
+
+            .then(function (resultado) {
+
+                res.json(resultado);
+
+            }).catch(function (erro) {
+
+                console.log(erro);
+
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+
+                res.status(500).json(erro.sqlMessage);
+
+            });
+    }
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+}
